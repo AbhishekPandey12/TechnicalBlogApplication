6d21548e31a10d330725ea36543a6760467e9509
abhishekkumarpandey <abhisonu3629@gmail.com>
2019-03-03 11:14:44 +0530

    Login & Logout Functionality

--- src/main/java/com/abhishek/controller/PostController.java
+++ src/main/java/com/abhishek/controller/PostController.java
@@ -4,28 +4,22 @@ import com.abhishek.model.Post;
 import com.abhishek.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
 @Controller
-public class HomeController {
-
-  public HomeController(){
-    System.out.println("** HomeController **");
-  }
+public class PostController {
 
   @Autowired
   private PostService postService;
 
-  @RequestMapping("/")
-  public String getAllPosts(Model model){
+  @RequestMapping("posts")
+  public String getUserPosts(Model model){
 
-    List<Post> posts = postService.getAllPosts();
-
-    model.addAttribute("posts", posts);
-
-    return "index";
+    List<Post> posts = postService.getOnePost();
+    model.addAttribute("userPost", posts);
+    return "posts";
   }
 }
